d56974d833b77c7ae1bebb0fa601218de5866585
Aaditya Kaushik <aadityakaushik834@gmail.com>
2023-10-23 18:49:49 +0530

    after more timepass

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def collision_for_object(player_pos_x,player_pos_y,object_pos_x,object_pos_y):
 	
 
 pg.init()
-pg.display.set_caption('Snake Game')
+
 screen=pg.display.set_mode((600,600))
 player_pos=pg.Vector2(screen.get_height()/2,screen.get_width()/2)
 fruit_pos=pg.Vector2(200,200)
@@ -29,16 +29,36 @@ running=True
 speed=15
 background = pg.image.load('/home/aadityakaushik/Downloads/hacker.jpeg') 
 snake_dict=[]
+font=pg.font.SysFont(None,30)
+
+length=1
 while running:
 	screen.fill('black')
 	screen.blit(background,(0,0))
-	pg.draw.circle(screen,'red',(player_pos.x,player_pos.y),15)
+	sctxt=font.render("Score:"+str(len(snake_dict)),True,"red")
+	screen.blit(sctxt,[20,20])
+#	pg.display.set_caption('Score:'+str(len(snake_dict)))
+	pg.draw.circle(screen,'red',(player_pos.x,player_pos.y),15)	
+	#for the real working comment the below code.
+	for i in range(length):
+			rn=random.randint(0,20)
+			if(rn<5):
+				pg.draw.circle(screen,'red',(player_pos.x+length,player_pos.y+length),15)	
+			elif(5<rn and rn<10):
+				pg.draw.circle(screen,'red',(player_pos.x-length,player_pos.y+length),15)	
+			elif(10<rn and rn<15):
+				pg.draw.circle(screen,'red',(player_pos.x+length,player_pos.y-length),15)
+			else:
+				pg.draw.circle(screen,'red',(player_pos.x-length,player_pos.y-length),15)
+
+
 	for i in snake_dict[:len(snake_dict)-1]:
 		pg.draw.circle(screen,'red',(i[0],i[1]),15)
 		if collision_for_object(player_pos.x,player_pos.y,i[0],i[1]) == 1:
 			print('GAME OVER !!')
 			running=False
 			sys.exit()
+	
 
 	keys = pg.key.get_pressed()
 	if keys[pg.K_w]:
@@ -73,7 +93,11 @@ while running:
 	if collision_for_object(player_pos.x,player_pos.y,fruit_pos.x,fruit_pos.y) == 1:
 		fruit_pos.x=random.randint(10,590)
 		fruit_pos.y=random.randint(10,590)
+		if [fruit_pos.x,fruit_pos.y] in snake_dict:
+			fruit_pos.x=random.randint(10,590)
+			fruit_pos.y=random.randint(10,590)
 		speed+=2
+		length+=1
 		snake_dict.append([player_pos.x,player_pos.y])
 	
 
